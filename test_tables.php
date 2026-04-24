ed9bfc021f6446e0a6c48c090310839f69997203
Rafael San‑José <rsanjose@alxarafe.com>
2026-04-24 20:08:09 +0200

    Cleanup: remove temporary test scripts

--- test_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-define('FS_FOLDER', __DIR__);
-require 'config.php';
-$db = new FacturaScripts\Core\Base\DataBase();
-$db->connect();
-$tables = $db->getTables();
-var_dump(in_array('codigos_postales', $tables));
--- test_wizard.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-define('FS_FOLDER', __DIR__);
-require 'config.php';
-$db = new FacturaScripts\Core\Base\DataBase();
-$db->connect();
-$tables = $db->getTables();
-echo "Number of tables: " . count($tables) . "\n";
-echo "Exists codigos_postales? " . ($db->tableExists('codigos_postales') ? 'yes' : 'no') . "\n";
