37584ab6224f07528da450b038ac2a7a36c008c3
Rafael San‑José <rsanjose@alxarafe.com>
2026-04-25 11:32:30 +0200

    fix: fix extracted plugins Init dependencies and method signatures

--- Plugins/Admin/Init.php
+++ Plugins/Admin/Init.php
@@ -2,13 +2,17 @@
 
 namespace FacturaScripts\Plugins\Admin;
 
-class Init extends \FacturaScripts\Core\Base\InitClass
+class Init extends \FacturaScripts\Core\Template\InitClass
 {
     public function init(): void
     {
     }
 
     public function update(): void
+    {
+    }
+
+    public function uninstall(): void
     {
     }
 }
