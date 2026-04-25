124e51351e5f1cd481c5822d2d93ec5ea831264c
Rafael San‑José <rsanjose@alxarafe.com>
2026-04-25 11:10:44 +0200

    refactor: extract Trading module from Core to Plugins using Hollow Bridge pattern

---
+++ Plugins/Trading/Init.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace FacturaScripts\Plugins\Trading;
+
+class Init extends \FacturaScripts\Core\Base\InitClass
+{
+    public function init(): void
+    {
+    }
+
+    public function update(): void
+    {
+    }
+}
